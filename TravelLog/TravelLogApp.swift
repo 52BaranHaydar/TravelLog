1f11d6ac0ae2e31eb4fc909c1b46d44108a34cd7
Baran Haydar <baranhaydar80@gmail.com>
2026-03-21 20:54:49 +0300

    Initial Commit

---
+++ TravelLog/TravelLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelLogApp.swift
+//  TravelLog
+//
+//  Created by Baran on 21.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct TravelLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
